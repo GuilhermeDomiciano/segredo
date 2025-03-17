e926ccb2aa89eec3ef2f2b98c892a10f46acc347
Domiciano <guilhermedomicianosilva@gmail.com>
2025-03-16 22:41:51 -0300

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Meu App Mobile",
+  description: "Aplicação otimizada para dispositivos móveis",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className="bg-gray-100 text-gray-900">{children}</body>
+    </html>
+  );
+}
